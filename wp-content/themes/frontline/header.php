ca15215ad9bbec4c69897a2290b4c4fbfcc61dc8
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-29 22:18:25 +0200

    Fikset headeren

--- wp-content/themes/frontline/header.php
+++ wp-content/themes/frontline/header.php
@@ -4,8 +4,7 @@
 	<title>Frontline Academy</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-	<link rel="stylesheet" href="<?php bloginfo('template_url')?>/bootstrap/css/bootstrap.css">
-
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootstrap/css/bootstrap.css">
 	<?php wp_head(); ?>
 </head>
 <body>
