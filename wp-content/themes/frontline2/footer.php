d78443add355c1e8a06b63a21eac11baac10fd1f
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-29 22:23:24 +0200

    bugfix

---
+++ wp-content/themes/frontline2/footer.php
@@ -0,0 +1,10 @@
+	<footer class="footer">
+		<div class="container">
+			<p class="text-muted credit">Laget av gruppe 1</p>
+		</div>
+	</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="<?php bloginfo('template_url')?>/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ wp-content/themes/frontline2/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontline Academy</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootstrap/css/bootstrap.css">
+	<?php wp_head(); ?>
+</head>
+<body>
