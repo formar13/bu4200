35363f645e33b67f7d6e6ac37ca06f66dec531c3
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-30 17:32:36 +0200

    Fikset wordpress-title

--- wp-content/themes/frontline/header.php
+++ wp-content/themes/frontline/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php the_title(); ?></title>
+	<title><?php bloginfo('name'); ?></title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootstrap/css/bootstrap.css">
