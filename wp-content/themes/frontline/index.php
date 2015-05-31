f5ff6334de10b943a4b17e0c7a2e1b4c7a6e9cd0
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-31 21:47:48 +0200

    Økt mellomrommet mellom postene

--- wp-content/themes/frontline/index.php
+++ wp-content/themes/frontline/index.php
@@ -40,7 +40,7 @@
 
                         <?php if (have_posts()) : ?>
                         <?php while (have_posts()) : the_post(); ?>
-                            <article <?php post_class('TESTCLASSEEEEEEEN'); ?>>
+                            <article <?php post_class('postArticle'); ?>>
                                 <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 
                                 <?php if (has_post_thumbnail()) : ?>
