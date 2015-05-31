0d5ef636f0f838a8dbd2dd2e660114bfeb181dd1
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-31 15:37:23 +0200

    Lagt til egen side for poster

---
+++ wp-content/themes/frontline/post.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fredrik
+ * Date: 31.05.2015
+ * Time: 15:37
+ */
+
+get_header();
+
+?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-xs-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+
+                        <?php if (have_posts()) : ?>
+                            <?php while (have_posts()) : the_post(); ?>
+
+                                <article>
+                                    <h2><?php the_title(); ?></h2>
+                                    <?php the_content(); ?>
+                                </article>
+
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php if(dynamic_sidebar('forside_widgetarea')) : else : endif ?>
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
