31b957f8d7a6d24265d854271fd419cac81f4f68
Fredrik <gabrielsenfredrik@gmail.com>
2015-05-31 02:23:14 +0200

    Oppdatert logoen

--- wp-content/themes/frontline/index.php
+++ wp-content/themes/frontline/index.php
@@ -13,7 +13,7 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			      </button>
-			      <a class="navbar-brand" href="#">
+			      <a class="navbar-brand" href="<?php echo get_option('home'); ?>">
 			      	<img alt="Frontline Academy" src="<?php bloginfo('template_url'); ?>/images/logo.png" height="50px">
 			      </a>
 			    </div>
