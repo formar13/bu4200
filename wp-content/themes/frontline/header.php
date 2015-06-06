2f0f3daf1ab89bdc63d97c711e7f3ca8ee535344
Fredrik <gabrielsenfredrik@gmail.com>
2015-06-06 10:24:16 +0200

    Lagt til favicon

--- wp-content/themes/frontline/header.php
+++ wp-content/themes/frontline/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootstrap/css/bootstrap.css">
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png">
 	<?php wp_head(); ?>
 </head>
 <body>
